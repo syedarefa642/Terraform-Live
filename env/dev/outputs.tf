c6f166609190c04ede7348ea6505de12a30fbeb6
syedarefa642 <syeda_refa@Refa.localdomain>
2026-06-15 14:25:39 +0000

    Added VPC IAM S3 EC2 Terraform configuration

--- env/dev/outputs.tf
+++ env/dev/outputs.tf
@@ -1,7 +1,23 @@
-output "instance_id" {
-  value = aws_instance.dev_server.id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "public_ip" {
-  value = aws_instance.dev_server.public_ip
+output "subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.ec2.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.bucket.bucket
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.ec2_role.name
 }
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,17 +1,16 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "dev"
+  default = "dev"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  default = "t3.micro"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
   type        = string
-  default     = "t3.micro"
 }
